e53b310ef8e4aab1b36abe4243837743c64bb3b4
Ahmad <ahmads20002000@gmail.com>
2024-05-04 16:53:30 +0300

    App comp use state to generate new word

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,24 @@
 import Drawing from "./components/Drawing/Drawing";
 import Word from "./components/Word/Word";
 import Keyboard from "./components/Keyboard/Keyboard";
+import { useEffect, useState } from "react";
 import "./App.css";
+import wordData from "./wordList.json";
 
 function App() {
+    const [randomWord, setRandomWord] = useState("");
+    const selectNewRandomWord = () => {
+        const newWord = wordData[Math.ceil(wordData.length * Math.random())];
+        setRandomWord(newWord);
+    };
+
+    useEffect(() => {
+        selectNewRandomWord();
+    }, []);
+
     return (
         <div className="container">
+            {randomWord}
             <Drawing />
             <Word />
             <Keyboard />
