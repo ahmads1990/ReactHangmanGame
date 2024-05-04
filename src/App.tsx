9fef130ea2b1c5907f921fbdfb039e21935c8b27
Ahmad <ahmads20002000@gmail.com>
2024-05-04 10:25:07 +0300

    Add hangman drawing

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
 import Drawing from "./components/Drawing/Drawing";
 import Word from "./components/Word/Word";
 import Keyboard from "./components/Keyboard/Keyboard";
+import "./App.css";
 
 function App() {
     return (
-        <div>
+        <div className="container">
             <Drawing />
             <Word />
             <Keyboard />
